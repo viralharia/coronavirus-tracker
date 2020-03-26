2bc4460453adb6679c1ec7c133f5ad8648fb21c7
Viral Haria <viralhar@gmail.com>
2020-03-26 07:30:11 +0530

    updated the CSV file url to a new url

--- src/main/java/com/vharia/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/vharia/coronavirustracker/services/CoronaVirusDataService.java
@@ -26,7 +26,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class CoronaVirusDataService {
 
-    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
         
     private List<LocationStats> allStats = new ArrayList<>();
     private String asOfDate;
